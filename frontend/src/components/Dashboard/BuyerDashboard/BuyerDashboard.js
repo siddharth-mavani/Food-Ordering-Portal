54ae44bc39e473afdf317c33d8229bd272e8ab81
Siddharth Mavani <siddharth.mavani@students.iiit.ac.in>
2022-01-25 03:08:44 +0530

    Created Buyer Dashboard

--- frontend/src/components/Dashboard/BuyerDashboard/BuyerDashboard.js
+++ frontend/src/components/Dashboard/BuyerDashboard/BuyerDashboard.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
@@ -11,37 +12,184 @@ import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
-import Divider from "@mui/material/Divider";
 import Autocomplete from "@mui/material/Autocomplete";
 import IconButton from "@mui/material/IconButton";
 import InputAdornment from "@mui/material/InputAdornment";
+import Fuse from 'fuse.js';
+import Chip from '@mui/material/Chip';
 
 import SearchIcon from "@mui/icons-material/Search";
 import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
 import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 
 const BuyerDashboard = (props) => {
-  const [users, setUsers] = useState([]);
-  const [sortedUsers, setSortedUsers] = useState([]);
-  const [sortName, setSortName] = useState(true);
+
+  const navigate = useNavigate();
+
+  const [food_items, setFoodItems] = useState([]);
+  const [filtered_food_items, setFilteredFoodItems] = useState([]);
+  const [sortedfood_items, setSortedFoodItems] = useState([]);
+  const [sortFoodName, setSortFoodName] = useState(true);
+  const [ShopNames, setShopNames] = useState([]);
+  const [Tags, setTags] = useState([]);
+  const [balance, setBalance] = useState(0);
+  
+  const [FilterShopName, setFilterShopName] = useState([]);
   const [searchText, setSearchText] = useState("");
+  const [DisplayTag, setDisplayTag] = useState("");
+  const [veg_nveg, setVegNveg] = useState("");
+  const [min_price, setMinPrice] = useState("");
+  const [max_price, setMaxPrice] = useState("");
+
 
   useEffect(() => {
     axios
-      .get("http://localhost:4000/buyer")
+      .get("http://localhost:4000/food")
       .then((response) => {
-        setUsers(response.data);
-        setSortedUsers(response.data);
+        setFoodItems(response.data);
+        setSortedFoodItems(response.data);
+        setFilteredFoodItems(response.data);
         setSearchText("");
+
+        const tags = [];
+        response.data.map((food_item) =>{
+          food_item.tags.map((tag) => {
+            if(!tags.includes(tag)){
+              tags.push(tag);
+            }
+          })
+        })
+        
+        setTags(tags);
+
+        const shops = [];
+        response.data.map((food_item) =>{
+         
+            if(!shops.includes(food_item.shop_name)){
+              shops.push(food_item.shop_name);
+            }
+          
+        })
+        
+        setShopNames(shops);
+      })
+      .catch((error) => {
+        console.log(error);
+    });
+    
+    const email = localStorage.getItem('BuyerEmail');
+
+    const getBuyer = {
+      email: email,
+    };
+
+    axios
+      .post("http://localhost:4000/buyer/getbuyer", getBuyer)
+      .then((response) =>{
+        setBalance(response.data.money);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
 
-  const sortChange = () => {
-    let usersTemp = users;
-    const flag = sortName;
+  }, []);
+  
+  useEffect(() => {
+    let result = food_items;
+
+    // Filter by min price
+    if(min_price){
+      const temp = [];
+      result.map((food_item) =>{
+        if(food_item.price >= min_price){
+          temp.push(food_item);
+        }
+      })
+
+      result = temp;
+    }
+    
+    // Filter by max price
+    if(max_price){
+      const temp = [];
+      result.map((food_item) =>{
+        if(food_item.price <= max_price){
+          temp.push(food_item);
+        }
+      })
+
+      result = temp;
+    }
+
+    // Filter by Shop Name
+    if(FilterShopName !== '' && FilterShopName !== null){
+      const temp = [];
+      result.map((food_item) =>{
+        if(food_item.shop_name === FilterShopName){
+          temp.push(food_item);
+        }
+      })
+
+      result = temp;
+    }
+
+    // Tags
+    if(DisplayTag !== '' && DisplayTag !== null){
+
+      const temp = [];
+      result.map((food_item) => {
+        food_item.tags.map((tag) => {
+          if(tag == DisplayTag){
+            temp.push(food_item);
+          }
+        })
+      })
+
+      result = temp;
+    }
+
+    // Veg/Non-Veg
+    if(veg_nveg !== '' && veg_nveg !== null){
+
+      const temp = [];
+      result.map((food_item) =>{
+        if(food_item.item_type === veg_nveg){
+          temp.push(food_item);
+        }
+      })
+
+      result = temp;
+    }
+    
+    // Fuzzy Search
+    const fuse = new Fuse(result, {
+      keys: ["item_name"],
+      threshold: 0.1,
+    });
+
+    const search_result = fuse.search(searchText);
+
+    const temp = [];
+
+    search_result.map((food_item) =>{
+      temp.push(food_item.item);
+    })
+
+    if(!(temp.length === 0 || searchText === "")){
+      result = temp;
+    }
+
+    setFilteredFoodItems(result);
+
+  }, [searchText, FilterShopName, DisplayTag, veg_nveg, min_price, max_price]);
+
+  const onChangeFilterShopName = (value) => {
+    setFilterShopName(value);
+  }
+
+  const sortChange = (event) => {
+    let usersTemp = food_items;
+    const flag = sortFoodName;
     usersTemp.sort((a, b) => {
       if (a.date != undefined && b.date != undefined) {
         return (1 - flag * 2) * (new Date(a.date) - new Date(b.date));
@@ -49,18 +197,48 @@ const BuyerDashboard = (props) => {
         return 1;
       }
     });
-    setUsers(usersTemp);
-    setSortName(!sortName);
+    setFoodItems(usersTemp);
+    setSortFoodName(!sortFoodName);
   };
 
-  const customFunction = (event) => {
-    console.log(event.target.value);
+  const onChangeDisplayTag = (value) => {
+
+    setDisplayTag(value);    
+
+  }
+
+  const onChangeVegNveg = (value) => {
+    
+    setVegNveg(value);
+
+  }
+
+  const onChangeSearch = (event) => {
+    
     setSearchText(event.target.value);
+   
   };
+
+  const onChangeMinPrice = (value) => {
+    setMinPrice(value);
+  };
+
+  const onChangeMaxPrice = (value) => {
+    setMaxPrice(value);
+  };
+
+  const GoToWallet = () => {
+    navigate("/wallet");
+  }
 
   return (
     <div>
-      <Grid container>
+      <Grid container item xs={12} md={3} lg={3}>
+        <Button variant="contained" onClick={GoToWallet}>Current Balance = {balance}</Button>
+      </Grid>
+      
+      <Grid item>
+        
         <Grid item xs={12} md={3} lg={3}>
           <List component="nav" aria-label="mailbox folders">
             <ListItem text>
@@ -68,7 +246,109 @@ const BuyerDashboard = (props) => {
             </ListItem>
           </List>
         </Grid>
-        <Grid item xs={12} md={9} lg={9}>
+
+        <Grid item xs={24} md={9} lg={9} aligncontent={"left"}>
+          <List component="nav" aria-label="mailbox folders" columnspacing={2}>
+            <ListItem xs={24}>
+
+              <Grid container spacing={2} md={3} lg={3}>
+
+                <Grid item xs={12}>
+                  Price
+                </Grid>
+
+                <Grid item xs={6}>
+                  <TextField
+                    id="standard-basic"
+                    label="Min"
+                    type="number"
+                    fullWidth={true}
+                    onChange={(event) => onChangeMinPrice(event.target.value)}
+                  />
+                </Grid>
+
+                <Grid item xs={6}>
+                  <TextField
+                    id="standard-basic"
+                    label="Max"
+                    type="number"
+                    fullWidth={true}
+                    onChange={(event) => onChangeMaxPrice(event.target.value)}
+                  />
+                </Grid> 
+              </Grid>
+
+              <Grid container spacing={2} md={3} lg={3} sx={{ml: 2}}>
+                <Grid item xs={12}>
+                  Shop Names
+                </Grid>
+                
+                <Autocomplete
+                  xs={12}
+                  id="combo-box-demo"
+                  options={ShopNames}
+                  onChange={(event, value) => onChangeFilterShopName(value)}
+                  getOptionLabel={(option) => option}
+                  fullWidth
+                  renderInput={(params) => (
+                    <TextField sx={{mt:2}}
+                      {...params}
+                      label="Select Shop Name"
+                      variant="outlined"
+                    />
+                  )}
+                />
+
+              </Grid>
+
+              <Grid container spacing={2} md={3} lg={3} sx={{ml: 2}}>
+                <Grid item xs={12}>
+                  Food Tags
+                </Grid>
+
+                <Autocomplete
+                  id="combo-box-demo"
+                  options={Tags}
+                  // value={DisplayTag}
+                  getOptionLabel={(option) => option}
+                  fullWidth
+                  onChange={(event, value) => onChangeDisplayTag(value)}
+                  renderInput={(params) => (
+                    <TextField sx={{mt:2}}
+                      {...params}
+                      label="Select Food Tags"
+                      variant="outlined"
+                    />
+                  )}
+                />
+              </Grid>
+
+              <Grid container spacing={2} md={3} lg={3} sx={{ml: 2}}>
+                <Grid item xs={12}>
+                  Veg/Non-Veg
+                </Grid>
+
+                <Autocomplete
+                  id="combo-box-demo"
+                  options={["Veg", "Non-Veg"]}
+                  getOptionLabel={(option) => option}
+                  fullWidth
+                  onChange={(event, value) => onChangeVegNveg(value)}
+                  renderInput={(params) => (
+                    <TextField sx={{mt:2}}
+                      {...params}
+                      label="Select Food Type"
+                      variant="outlined"
+                    />
+                  )}
+                />
+              </Grid>
+
+            </ListItem>
+          </List>
+        </Grid>
+
+        <Grid item xs={12} md={9} lg={12}>
           <List component="nav" aria-label="mailbox folders">
             <TextField
               id="standard-basic"
@@ -83,77 +363,72 @@ const BuyerDashboard = (props) => {
                   </InputAdornment>
                 ),
               }}
-              // onChange={customFunction}
+              onChange={onChangeSearch}
             />
           </List>
         </Grid>
       </Grid>
-      <Grid container>
-        <Grid item xs={12} md={3} lg={3}>
-          <List component="nav" aria-label="mailbox folders">
-            <ListItem>
-              <Grid container spacing={2}>
-                <Grid item xs={12}>
-                  Salary
-                </Grid>
-                <Grid item xs={6}>
-                  <TextField
-                    id="standard-basic"
-                    label="Enter Min"
-                    fullWidth={true}
-                  />
-                </Grid>
-                <Grid item xs={6}>
-                  <TextField
-                    id="standard-basic"
-                    label="Enter Max"
-                    fullWidth={true}
-                  />
-                </Grid>
-              </Grid>
-            </ListItem>
-            <Divider />
-            <ListItem divider>
-              <Autocomplete
-                id="combo-box-demo"
-                options={users}
-                getOptionLabel={(option) => option.name}
-                fullWidth
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    label="Select Names"
-                    variant="outlined"
-                  />
-                )}
-              />
-            </ListItem>
-          </List>
-        </Grid>
-        <Grid item xs={12} md={9} lg={9}>
+      <Grid container align={"left"}>
+        
+        <Grid item xs={12} md={9} lg={12}>
           <Paper>
             <Table size="small">
               <TableHead>
                 <TableRow>
                   <TableCell> Sr No.</TableCell>
+                  <TableCell>Shop Name</TableCell>
+                  <TableCell>Item Name</TableCell>
                   <TableCell>
-                    {" "}
+                    {"Price"}
                     <Button onClick={sortChange}>
-                      {sortName ? <ArrowDownwardIcon /> : <ArrowUpwardIcon />}
+                      {sortFoodName ? <ArrowDownwardIcon /> : <ArrowUpwardIcon />}
                     </Button>
-                    Date
                   </TableCell>
-                  <TableCell>Name</TableCell>
-                  <TableCell>Email</TableCell>
+                  <TableCell>Item Type</TableCell>
+                  <TableCell>Addons</TableCell>
+                  <TableCell>Tags</TableCell>
+                  <TableCell>Rating</TableCell>
+                  <TableCell> </TableCell>
+                  <TableCell> </TableCell>
                 </TableRow>
               </TableHead>
               <TableBody>
-                {users.map((user, ind) => (
+                {filtered_food_items.map((food_item, ind) => (
                   <TableRow key={ind}>
-                    <TableCell>{ind}</TableCell>
-                    <TableCell>{user.date}</TableCell>
-                    <TableCell>{user.name}</TableCell>
-                    <TableCell>{user.email}</TableCell>
+                    <TableCell>{ind+1}</TableCell>
+                    <TableCell>{food_item.shop_name}</TableCell>
+                    <TableCell>{food_item.item_name}</TableCell>
+                    <TableCell>{food_item.price}</TableCell> 
+                    <TableCell>{food_item.item_type}</TableCell>  
+                    <TableCell>
+                      <ul class="unstyled">
+                      {
+                      food_item.addons.map((addon) => {
+                        return <li>{addon.name}</li>
+                      })
+                      }
+                      </ul>
+                    </TableCell> 
+                    <TableCell>
+                    <ul class="unstyled">
+                      {
+                      food_item.tags.map((tag) => {
+                        return <li>{tag}</li>
+                      })
+                      }
+                      </ul>
+                    </TableCell>                
+                    <TableCell>{food_item.rating}</TableCell>
+                    <TableCell>
+                      <Button onClick={sortChange}>
+                        {sortFoodName ? <ArrowDownwardIcon /> : <ArrowUpwardIcon />}
+                      </Button> 
+                    </TableCell>
+                    <TableCell> 
+                      <Button onClick={sortChange}>
+                        {sortFoodName ? <ArrowDownwardIcon /> : <ArrowUpwardIcon />}
+                      </Button>
+                    </TableCell>
                   </TableRow>
                 ))}
               </TableBody>
